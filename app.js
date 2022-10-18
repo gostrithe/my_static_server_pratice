56c4207f29b3fd458172cf39ca80dc9e440e5b6b
gostrithe <11775651+gostrithe@user.noreply.gitee.com>
2022-10-18 20:16:19 +0800

    get pics, static resources done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* 导入模块 */
 // Node.js 提供了 http 模块，http 模块主要用于搭建 HTTP 服务端和客户端
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const mathApi = require('./routers/math');
 
@@ -13,6 +15,20 @@ const requestListener = (request, response) => {
             response.end(JSON.stringify({
                 result
             }));
+            break;
+
+        case request.url.startsWith('/file/'):
+            const reg2 = /\/file\/(.*)/;
+            const picFileName = reg2.exec(request.url)[1];
+            // __dirname全局变量 表示当前执行脚本所在的目录
+            // D:\桌面\H5二阶段\我的二阶段\my_static_server_pratice
+            console.log(__dirname);
+            const joinPath = path.join(__dirname, 'public', 'img', picFileName);
+            console.log(joinPath);
+            const picBuffer = fs.readFileSync(joinPath)
+            console.log(picBuffer);
+
+            response.end(picBuffer);
             break;
 
         default:
---
+++ routers/file.js
@@ -0,0 +1,5 @@
+/* 导入文件系统模块 文件操作API */
+const fs = require('fs');
+/* Node.js path 模块提供了一些用于处理文件路径的小工具 */
+const path = require('path');
+
